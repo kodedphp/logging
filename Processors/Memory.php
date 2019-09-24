657d1eb8576cb60b8e41ce5b8becfe2cfb7d5e7c
Mihail Binev <kodeart@gmail.com>
2019-09-24 09:12:45 +0200

    - adds return type - adds the licence

--- Processors/Memory.php
+++ Processors/Memory.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Koded package.
+ *
+ * (c) Mihail Binev <mihail@kodeart.com>
+ *
+ * Please view the LICENSE distributed with this source code
+ * for the full copyright and license information.
+ *
+ */
+
 namespace Koded\Logging\Processors;
 
 /**
@@ -8,8 +18,7 @@ namespace Koded\Logging\Processors;
  */
 class Memory extends Processor
 {
-
-    protected function parse(array $message)
+    protected function parse(array $message): void
     {
         $this->formatted .= PHP_EOL . strtr($this->format, $message);
     }
